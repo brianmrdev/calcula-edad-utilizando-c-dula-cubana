25b602af1331f8485f5cdff82c12275a653b4961
Brian Martínez Rodríguez <44876509+brianmrdev@users.noreply.github.com>
2023-02-08 12:11:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ import datetime
 from dateutil.relativedelta import relativedelta
 
 def calcular_edad(ci):
-    
-    fecha_nac = ci[:6]
-    fecha = fecha_nac[:2] + fecha_nac[2:4] + fecha_nac[-2:]
-    ordenar_fecha= datetime.datetime.strptime(fecha, '%y%m%d')
-    
-    fecha_actual = datetime.datetime.now().date()
-    
-    if ordenar_fecha.year > fecha_actual.year:
-        ordenar_fecha = ordenar_fecha.replace(year=ordenar_fecha.year-100)
-    
-    edad = relativedelta(datetime.datetime.now(), ordenar_fecha.date())
-    
-    print(f"{edad.years} años")
+    if ci.isdigit():
+        if len(ci) == 11:
+            fecha_nac = ci[:6]
+            fecha = fecha_nac[:2] + fecha_nac[2:4] + fecha_nac[-2:]
+            ordenar_fecha= datetime.datetime.strptime(fecha, '%y%m%d')
+            
+            fecha_actual = datetime.datetime.now().date()
+            
+            if ordenar_fecha.year > fecha_actual.year:
+                ordenar_fecha = ordenar_fecha.replace(year=ordenar_fecha.year-100)
+            
+            edad = relativedelta(datetime.datetime.now(), ordenar_fecha.date())
+            
+            print(f"{edad.years} años")
+        
+        else:
+            print("El número de cedula debe tener 11 dígitos")
+    else:
+        print("El número de cedula debe contener solo dígitos")
     
 
-calcular_edad(ci = "88081128104")
+calcular_edad(ci = input("Introduce Numero de Identidad: "))
